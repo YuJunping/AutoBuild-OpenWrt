ba478c009eefc9af6b6e7634c5b8f7d27e4aac8c
YuJunping <42385360@qq.com>
2020-09-06 12:29:12 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,3 +5,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/172.31.0.10/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/%D.*$/OpenWrt AutoBuild by LostNetwork/' openwrt/package/base-files/files/etc/banner
